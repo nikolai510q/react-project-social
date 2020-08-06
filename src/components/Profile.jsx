cfd3736479fd725764e5db39fa5c0668630e8288
Николай Левченков <nikolai510q@gmail.com>
2020-08-06 15:01:13 +0300

    add css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
-            <div>
+        <div className={s.content}>
+            <div className={s.item}>
                 <img src='https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701372504.jpg' width='900px' />
             </div>
-            <div>
+            <div >
                 ava + description
         </div>
             <div>
                 My posts
-          <div>
+          <div className={s.posts}>
                     New post
           </div>
                 <div>
-                    <div>
+                    <div className={s.item}>
                         post1
             </div>
-                    <div>
+                    <div className={s.item}>
                         post2
             </div>
                 </div>
