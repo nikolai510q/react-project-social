5d54fe79aa2b9e33f5364ef8e60c24a369633c4b
Николай Левченков <nikolai510q@gmail.com>
2020-08-06 13:03:10 +0300

    commit on 'master'

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://img2.freepng.ru/20180920/iiu/kisspng-google-chrome-portable-network-graphics-computer-i-chrome-svg-png-icon-free-download-295963-onli-5ba442ac7399b0.4332403515374916284735.jpg' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701372504.jpg' width='900px' />
+            </div>
+            <div>
+                ava + description
+        </div>
+            <div>
+                My posts
+          <div>
+                    New post
+          </div>
+                <div>
+                    <div>
+                        post1
+            </div>
+                    <div>
+                        post2
+            </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
